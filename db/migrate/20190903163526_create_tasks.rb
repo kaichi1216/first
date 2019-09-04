4e68bbef47906a93532a7fdf0f222f2349108af1
kai <jok002787@gmail.com>
2019-09-04 13:08:35 +0800

    revice migrate L5

--- db/migrate/20190903163526_create_tasks.rb
+++ db/migrate/20190903163526_create_tasks.rb
@@ -2,7 +2,7 @@ class CreateTasks < ActiveRecord::Migration[5.2]
   def change
     create_table :tasks do |t|
       t.string :name
-      t.integer :date
+      t.datetime :date
       t.text :note
       t.timestamps
     end
